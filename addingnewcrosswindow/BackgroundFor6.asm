969c9eeed67e0548f3811ee1641e632acb926b16
Pulok Rahman <timetravelingknight@gmail.com>
2024-05-11 20:20:28 -0700

    Upgraded the crosswindow logic with bitflags

--- addingnewcrosswindow/BackgroundFor6.asm
+++ addingnewcrosswindow/BackgroundFor6.asm
@@ -8,5 +8,24 @@ OrginalCrossBackground:
 sub r0,1
 pop r15
 .pool
+BitTestWindow:
+push r14
+mov r0,0x1
+lsl r0,r4
+ldr r1,=0x56
+ldrb r1,[r5,r1]
+tst r0,r1 
+pop r15
+.pool
 
-
+BitSetWindow:
+push r14
+mov r0,0x1
+lsl r0,r4
+ldr r1,=0x56
+ldrb r1,[r5,r1]
+ORR r0,r1
+ldr r1,=0x56
+strb r0,[r5,r1]
+pop r15
+.pool
